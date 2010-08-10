e6c13610398f98120972ac7c3fe14ac9a8389764
Josh Starcher <josh.starcher@gmail.com>
2010-08-09 19:42:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sp2::Application.config.secret_token = '0d56be28e8035a71101323a863e0cf735fef274f008eb2caa161a838f02f9e1c5bbbd2a14dc13051472aaad2923a99249e7007fa4181a3513f281a5d92dd5b25'
