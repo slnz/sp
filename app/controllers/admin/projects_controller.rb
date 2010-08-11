c7a8649584c9b98d38227d391ff9957ad4e98d21
Josh Starcher <josh.starcher@gmail.com>
2010-08-11 10:52:19 -0500

    fix pagination per_page code

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,13 +1,8 @@
 class Admin::ProjectsController < ApplicationController
   layout 'admin'
   def index
-    if params[:projects_per_page]
-      cookies.delete(:projects_per_page)
-      cookies.permanent[:projects_per_page] = params[:projects_per_page] 
-    end
-    raise cookies[:projects_per_page].inspect
+    cookies.permanent[:projects_per_page] = params[:projects_per_page] if params[:projects_per_page]
     @per_page = cookies[:projects_per_page] || 20
-    raise @per_page.inspect
     @projects = SpProject.current.includes(:apd, :opd, :pd).paginate(:page => params[:page], :per_page => @per_page)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,15 @@ module ApplicationHelper
     END
     ret.html_safe
   end
+  
+  def sort_by(column, title = column.titleize, options = {})
+    if title.is_a?(Hash)
+      options = title 
+      title = column.titleize
+    end
+    ordered = params.dup
+    ordered[:direction] = (ordered[:order] == column && ordered[:direction] == 'asc') ? 'desc' : 'asc'
+    ordered[:order] = column
+    link_to(title, ordered)
+  end
 end
