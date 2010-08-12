cd60a632f584751e23c358c1513e0ed3409b1c74
Josh Starcher <josh.starcher@gmail.com>
2010-08-12 10:10:14 -0500

    add edit project page
    add questionnaire engine

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,8 +1,18 @@
 class Admin::ProjectsController < ApplicationController
+  before_filter :get_project, :only => [:edit, :destroy, :update]
   layout 'admin'
   def index
     cookies.permanent[:projects_per_page] = params[:projects_per_page] if params[:projects_per_page]
     @per_page = cookies[:projects_per_page] || 20
     @projects = SpProject.current.includes(:apd, :opd, :pd).paginate(:page => params[:page], :per_page => @per_page)
+  end
+  
+  def edit
+    
+  end
+  
+  protected 
+  def get_project
+    @project = SpProject.find(params[:id])
   end
 end
