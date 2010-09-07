c5765302d56fbe7dd680f1f055ea55f9c2d2247f
Twinge <josh.starcher@gmail.com>
2010-09-07 08:34:53 -0500

    more classes

--- db/migrate/20100902194905_add_person_id_to_sp_user.rb
+++ db/migrate/20100902194905_add_person_id_to_sp_user.rb
@@ -1,9 +1,21 @@
 class SpUser < ActiveRecord::Base
 end
+class SpNationalCoordinator < ActiveRecord::Base
+end
+class SpRegionalCoordinator < ActiveRecord::Base
+end
+class SpDirector < ActiveRecord::Base
+end
+class SpEvaluator < ActiveRecord::Base
+end
+class SpGeneralStaff < ActiveRecord::Base
+end
+class SpProjectStaff < ActiveRecord::Base
+end
 
 class AddPersonIdToSpUser < ActiveRecord::Migration
   def self.up
-    add_column :sp_users, :person_id, :integer
+    # add_column :sp_users, :person_id, :integer
     SpUser.all.each do |sp_user|
       sp_user.update_attribute(:person_id, sp_user.user.try(:person).try(:id))
     end
