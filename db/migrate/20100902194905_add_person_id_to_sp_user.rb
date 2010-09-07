2d9163a14fbfa3fecde2273d2bdca415a9fefdb6
Twinge <josh.starcher@gmail.com>
2010-09-07 08:58:46 -0500

    re-enable migration column

--- db/migrate/20100902194905_add_person_id_to_sp_user.rb
+++ db/migrate/20100902194905_add_person_id_to_sp_user.rb
@@ -27,7 +27,7 @@ end
 
 class AddPersonIdToSpUser < ActiveRecord::Migration
   def self.up
-    # add_column :sp_users, :person_id, :integer
+    add_column :sp_users, :person_id, :integer
     SpUser.all.each do |sp_user|
       sp_user.update_attribute(:person_id, sp_user.user.try(:person).try(:id))
     end
