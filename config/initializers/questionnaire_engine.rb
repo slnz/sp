9f5198e0ade43372a78677865ce1924492b5bd5f
Josh Starcher <josh.starcher@gmail.com>
2010-09-09 10:31:00 -0500

    update QE

---
+++ config/initializers/questionnaire_engine.rb
@@ -0,0 +1,9 @@
+module Questionnaire
+  # prefix for database tables
+  mattr_accessor :table_name_prefix
+  self.table_name_prefix = 'sp_'
+  
+  mattr_accessor :answer_sheet_has_one
+  self.answer_sheet_has_one = nil
+    
+end
