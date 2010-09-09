3c3e173d5885c767576a61636350b9ba281ca625
david <david@davids-Mac-mini.local>
2010-09-09 12:39:06 -0700

    picture field on projects

--- db/migrate/20081023162920_qe_initial.rb
+++ db/migrate/20081023162920_qe_initial.rb
@@ -24,7 +24,7 @@ class QeInitial < ActiveRecord::Migration
       t.column :page_id, :integer, :null => false
       t.column :kind, :string, :limit => 40, :null => false     # single table inheritance: class name
       t.column :style, :string, :limit => 40    # render style
-
+    
       t.column :label, :string, :limit => 255   # question label, section heading
       t.column :content, :text, :null => true   # for content/instructions, and for choices (one per line)
       
@@ -37,7 +37,7 @@ class QeInitial < ActiveRecord::Migration
       # t.foreign_key :question_sheet_id, QuestionSheet.table_name, :id
       # t.foreign_key :page_id, Page.table_name, :id
     end
-  
+      
     add_index Element.table_name, :slug
     add_index Element.table_name, [:question_sheet_id, :position, :page_id], :unique => false
     
