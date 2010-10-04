13534c1caf2742e0da2df75adb2ed4b5478bf29b
Josh Starcher <josh.starcher@gmail.com>
2010-10-04 09:52:21 -0500

    update QE

--- public/javascripts/rails.extra.js
+++ public/javascripts/rails.extra.js
@@ -1,30 +1,43 @@
 (function($) {
 	// Enable sortable items
 	$(function() {
+		setUpJsHelpers();
+	});			
+})(jQuery);
+
+function setUpSortables() {
+	$('[data-sortable]').sortable({axis:'y', 
+																  dropOnEmpty:false, 
+																  update: function(event, ui) {
+																		sortable = this;
+																		$.ajax({data:$(this).sortable('serialize',{key:sortable.id + '[]'}),
+																						complete: function(request) {$(sortable).effect('highlight')}, 
+																						success:function(request){$('#errors').html(request)}, 
+																						type:'put', 
+																						url: $(sortable).attr('data-sortable-url')
+																					 })
+																		}
+	});
+	$('[data-sortable][data-sortable-handle]').each(function() {
+		handle = $(this).attr('data-sortable-handle');
+		$(this).sortable("option", "handle", handle);
+	});
+}
+
+function setUpCalendars() {
+	$('[data-calendar]').datepicker({changeYear:true,
+																	 yearRange: '1975:c+5'})
+}
+
+function setUpJsHelpers() {
+	
 		// ==================
 		// Sortable
-		$('[data-sortable]').sortable({axis:'y', 
-																	  dropOnEmpty:false, 
-																	  update: function(event, ui) {
-																			sortable = this;
-																			$.ajax({data:$(this).sortable('serialize',{key:sortable.id + '[]'}),
-																							complete: function(request) {$(sortable).effect('highlight')}, 
-																							success:function(request){$('#errors').html(request)}, 
-																							type:'put', 
-																							url: $(sortable).attr('data-sortable-url')
-																						 })
-																			}
-																		})
-		$('[data-sortable][data-sortable-handle]').each(function() {
-			handle = $(this).attr('data-sortable-handle');
-			$(this).sortable("option", "handle", handle);
-		});
+		setUpSortables();
 		// ==================
 		
 		// ==================
 		// Calendar
-    $('[data-calendar]').datepicker({changeYear:true,
-																		 yearRange: '1975:c+5'})
+		setUpCalendars();
   	// ==================
-	});			
-})(jQuery);
+}
