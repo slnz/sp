87499d727a2c55630c0779ed8c50977abd0c026b
Josh Starcher <josh.starcher@gmail.com>
2010-10-12 08:24:53 -0500

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101011163404) do
+ActiveRecord::Schema.define(:version => 20101012131931) do
 
   create_table "academic_departments", :force => true do |t|
     t.string "name"
@@ -4446,7 +4446,7 @@ ActiveRecord::Schema.define(:version => 20101011163404) do
     t.boolean "hidden",                           :default => false
   end
 
-  add_index "sp_pages", ["question_sheet_id", "number"], :name => "page_number", :unique => true
+  add_index "sp_pages", ["question_sheet_id", "number"], :name => "page_number"
 
   create_table "sp_pages_depreated", :force => true do |t|
     t.string   "title",         :limit => 50
