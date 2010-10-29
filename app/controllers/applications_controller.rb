f617135c7536e36981fc90415b57fb040a37ccef
Twinge <josh.starcher@gmail.com>
2010-10-29 10:34:26 -0500

    ssm should be the first before filter to run

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationsController < AnswerSheetsController
-  before_filter :ssm_login_required, :except => [:closed]
+  prepend_before_filter :ssm_login_required, :except => [:closed]
   before_filter :redirect_to_closed, :except => [:closed]
-  before_filter :get_application, :only => [:multiple_projects, :done, :show]
   
   def closed
     render :layout => false
