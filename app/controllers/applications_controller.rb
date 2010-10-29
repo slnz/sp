e96e4346890849370ce79cc689179a39cc2133ac
Twinge <josh.starcher@gmail.com>
2010-10-29 10:50:57 -0500

    need that before filter

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationsController < AnswerSheetsController
   prepend_before_filter :ssm_login_required, :except => [:closed]
   before_filter :redirect_to_closed, :except => [:closed]
+  before_filter :get_application, :only => [:multiple_projects, :done, :show]
   
   def closed
     render :layout => false
