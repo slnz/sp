7d05c069b45dcbdb47f7ce947db380b0b84bb8fd
Josh Starcher <josh.starcher@gmail.com>
2010-10-31 15:43:30 -0500

    put instructions page first

--- app/presenters/answer_pages_presenter.rb
+++ app/presenters/answer_pages_presenter.rb
@@ -7,6 +7,11 @@ class AnswerPagesPresenter < Presenter
         @pages << page
       end
     end
+    # put the instructions page first
+    index = @pages.index {|p| p.label == 'Instructions'}
+    instructions = @pages.delete_at(index)
+    @pages.insert(0, instructions) if instructions
+    
     if answer_sheet.project.project_specific_question_sheet && answer_sheet.project.project_specific_question_sheet.pages.first && answer_sheet.project.project_specific_question_sheet.pages.first.elements.present?
       @pages.insert(-2, answer_sheet.project.project_specific_question_sheet.pages.first)
     end
