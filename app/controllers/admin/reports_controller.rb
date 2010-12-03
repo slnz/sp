d10770f8c50c0f98f2dd5741ca44adaff8cdf891
Josh Starcher <josh.starcher@gmail.com>
2010-12-03 17:38:38 -0500

    class for openings

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,8 +1,10 @@
 class Admin::ReportsController < ApplicationController
-  before_filter CASClient::Frameworks::Rails::Filter, AuthenticationFilter
+  before_filter CASClient::Frameworks::Rails::Filter, AuthenticationFilter, :check_access
   layout 'admin'
   def show
-    
+    if sp_user.is_a?(SpDirector)
+      redirect_to :director and return
+    end
   end
 
   def director
@@ -25,35 +27,43 @@ class Admin::ReportsController < ApplicationController
   end
 
   def male_openings
-    @percentages = {'0-50%' => [], '51-99%' => [], '100%' => []}
+    @percentages = {'0-50' => [], '51-99' => [], '100' => []}
     SpProject.current.uses_application.order(:name).each do |project|
       case 
       when project.percent_full_men < 50
-        @percentages['0-50%'] << project
+        @percentages['0-50'] << project
       when project.percent_full_men < 100
-        @percentages['51-99%'] << project
+        @percentages['51-99'] << project
       else
-        @percentages['100%'] << project
+        @percentages['100'] << project
       end
     end
   end
 
   def female_openings
-    @percentages = {'0-50%' => [], '51-99%' => [], '100%' => []}
+    @percentages = {'0-50' => [], '51-99' => [], '100' => []}
     SpProject.current.uses_application.order(:name).each do |project|
       case 
       when project.percent_full_women < 50
-        @percentages['0-50%'] << project
+        @percentages['0-50'] << project
       when project.percent_full_women < 100
-        @percentages['51-99%'] << project
+        @percentages['51-99'] << project
       else
-        @percentages['100%'] << project
+        @percentages['100'] << project
       end
     end
+  end
+
+  def ministry_focus
+    @focuses = SpMinistryFocus.order(:name)
   end
 
   protected
     def get_applications(project)
      SpApplication.preferrenced_project(project.id).for_year(project.year).order('ministry_person.lastName, ministry_person.firstName').includes(:person => :current_address)
+    end
+
+    def check_access
+      
     end
 end
