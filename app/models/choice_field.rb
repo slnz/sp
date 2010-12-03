c4f46a13ce852ca004566eca10aa46c0471bab15
Josh Starcher <josh.starcher@gmail.com>
2010-12-03 11:08:00 -0500

    add project preferences

---
+++ app/models/choice_field.rb
@@ -0,0 +1,24 @@
+require_dependency Rails.root.join('vendor','plugins','questionnaire_engine','app','models','choice_field').to_s
+class ChoiceField < Question
+
+  def ptemplate
+    if self.style == 'checkbox'
+      'checkbox_field'
+    elsif self.style == 'drop-down'
+      'drop_down_field'
+    elsif self.style == 'radio'
+      'radio_button_field'
+    elsif self.style == 'yes-no'
+      'yes_no'
+    elsif self.style == 'rating'
+      'rating'
+    elsif self.style == 'acceptance'
+      'acceptance'
+    elsif self.style == 'country'
+      'country'
+    elsif self.style == 'project-preference'
+      'project_preference'
+    end
+	end
+
+end
