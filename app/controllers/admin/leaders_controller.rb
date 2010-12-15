842fd8b8b181766dc8e593b8c3ebdabc419e037a
Josh Starcher <josh.starcher@gmail.com>
2010-12-15 14:48:54 -0500

    [SP-260]

--- app/controllers/admin/leaders_controller.rb
+++ app/controllers/admin/leaders_controller.rb
@@ -19,12 +19,12 @@ class Admin::LeadersController < ApplicationController
   
   def create
     @year = params[:year].present? ? params[:year] : @project.year
-    @person ||= Person.find(params[:person_id])
+    @person ||= Person.find(params[:person_id]) if params[:person_id]
     if ['apd','pd','opd','coordinator'].include?(params[:leader])
       @project.send(params[:leader] + '=', @person.id)
       @project.save(:validate => false)
     elsif ['staff','kid','volunteer', 'evaluator'].include?(params[:leader])
-      @project.sp_staff.create(:type => params[:leader].titleize, :year => @year, :person_id => params[:person_id])
+      @project.sp_staff.create(:type => params[:leader].titleize, :year => @year, :person_id => @person.id)
     end
     render :create
   end
@@ -39,7 +39,9 @@ class Admin::LeadersController < ApplicationController
     @current_address = CurrentAddress.new(params[:person].delete(:current_address).merge({:addressType => 'current'}))
     @person = Person.new(params[:person])
     @person.current_address = @current_address
-    unless [@person.firstName, @person.lastName, @person.gender, @current_address.homePhone, @current_address.email].all?(&:present?) && @person.valid? && @current_address.valid?
+    required_fields = [@person.firstName, @person.lastName, @person.gender]
+    required_fields += [@current_address.homePhone, @current_address.email] unless params[:leader] == 'kid'
+    unless required_fields.all?(&:present?) && @person.valid? && @current_address.valid?
       flash[:error] = "Please fill in all fields"
       errors = @person.errors.full_messages + @current_address.errors.full_messages
       flash[:error] = errors.join("<br />") if errors.present?
