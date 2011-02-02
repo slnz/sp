fe65573e7cc6b66f49acc3179bae8d3f2210fc5a
Josh Starcher <josh.starcher@gmail.com>
2011-02-02 10:47:03 -0500

    give evaluators access to search

--- app/models/sp_evaluator.rb
+++ app/models/sp_evaluator.rb
@@ -2,6 +2,7 @@ class SpEvaluator < SpUser
   def can_change_project_status?() false; end
   def can_change_self?() false; end
   def can_add_user?() false; end
+  def can_search?() true; end
   def can_see_other_regions?() false; end
   def can_evaluate_applicant?(app=nil)
     return false unless app
