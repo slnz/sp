03d378022dda739fb10d86f18e54f86c2f62b09d
Justin Sabelko <justin.sabelko@uscm.org>
2011-10-31 23:50:07 -0500

    Open application

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Sp2::Application.configure do
   config.reload_plugins = true 
   
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
