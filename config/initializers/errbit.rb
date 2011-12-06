d3670b77d1a511e76f46eed87d4e39690928b88a
Josh Starcher <josh.starcher@gmail.com>
2011-12-06 08:42:53 -0500

    use Airbrake instead of HoptoadNotifier

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,4 +1,4 @@
-HoptoadNotifier.configure do |config|
+Airbrake.configure do |config|
    config.api_key = 'a198e469967c9f50d7067551a0dc2c52'
    config.host    = 'errors.uscm.org'
    config.port    = 80
