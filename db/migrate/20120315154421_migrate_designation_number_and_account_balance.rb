f8de98e7aa7ee342c186283196802c1cdc7220e1
Josh Starcher <josh.starcher@gmail.com>
2012-03-23 17:21:52 -0400

    use AR:B for queries

--- db/migrate/20120315154421_migrate_designation_number_and_account_balance.rb
+++ db/migrate/20120315154421_migrate_designation_number_and_account_balance.rb
@@ -1,6 +1,6 @@
 class MigrateDesignationNumberAndAccountBalance < ActiveRecord::Migration
   def self.up
-    SpDesignationNumber.connection.select_all("SELECT * FROM sp_applications WHERE designation_number <> 0 AND designation_number IS NOT NULL AND person_id IS NOT NULL").each do |row|
+    ActiveRecord::Base.connection.select_all("SELECT * FROM sp_applications WHERE designation_number <> 0 AND designation_number IS NOT NULL AND person_id IS NOT NULL").each do |row|
       
       if row['project_id'] || row['preference1_id'] || row['preference2_id'] || row['preference3_id'] || row['preference4_id'] || row['preference5_id']
         unless project_id = row['project_id']
@@ -18,12 +18,12 @@ class MigrateDesignationNumberAndAccountBalance < ActiveRecord::Migration
         end
         # puts "PROJ:#{row['project_id']} PREF1:#{row['preference1_id']} PREF2:#{row['preference2_id']} PREF3:#{row['preference3_id']} PREF4:#{row['preference4_id']} PREF5:#{row['preference5_id']} = #{project_id}"
         
-        SpDesignationNumber.connection.insert("INSERT INTO sp_designation_numbers (person_id, project_id, designation_number, account_balance, created_at, updated_at) VALUES ( #{row['person_id']}, #{project_id}, #{row['designation_number']}, #{row['account_balance'] || 0}, NOW(), NOW())")
+        ActiveRecord::Base.connection.insert("INSERT INTO sp_designation_numbers (person_id, project_id, designation_number, account_balance, created_at, updated_at) VALUES ( #{row['person_id']}, #{project_id}, #{row['designation_number']}, #{row['account_balance'] || 0}, NOW(), NOW())")
       end
     end
   end
 
   def self.down
-    SpDesignationNumber.connection.execute("TRUNCATE sp_designation_numbers");
+    ActiveRecord::Base.connection.execute("TRUNCATE sp_designation_numbers");
   end
 end
