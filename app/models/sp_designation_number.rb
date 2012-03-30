cd44d3f8f2db2c38bb1aee8e5396e078c563ee30
Josh Starcher <josh@26am.com>
2012-03-29 22:00:36 -0400

    update how donations pull in

--- app/models/sp_designation_number.rb
+++ app/models/sp_designation_number.rb
@@ -2,6 +2,6 @@ class SpDesignationNumber < ActiveRecord::Base
   
   belongs_to :person
   belongs_to :project, :class_name => 'SpProject'
-  
+  has_many :donations, :class_name => "SpDonation", :primary_key => "designation_number", :order => 'donation_date desc'
   
 end
