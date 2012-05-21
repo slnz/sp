5a5c800208cab701b78e53efb3a3db48ceb95afc
Josh Starcher <josh.starcher@gmail.com>
2012-05-21 13:41:12 -0400

    move sp_designation_number.rb to CE

--- app/models/sp_designation_number.rb
+++
@@ -1,7 +0,0 @@
-class SpDesignationNumber < ActiveRecord::Base
-  
-  belongs_to :person
-  belongs_to :project, :class_name => 'SpProject'
-  has_many :donations, :class_name => "SpDonation", :primary_key => "designation_number", :order => 'donation_date desc'
-  
-end
