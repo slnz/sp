27fd1b09479642847b5d067a0e68400a6b83f706
Josh Starcher <josh.starcher@gmail.com>
2012-05-28 12:09:32 -0400

    no Devise in sp

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,7 +46,7 @@ Spork.prefork do
     config.treat_symbols_as_metadata_keys_with_true_values = true
     config.filter_run :focus => true
     config.run_all_when_everything_filtered = true
-    config.include Devise::TestHelpers, :type => :controller
+    #config.include Devise::TestHelpers, :type => :controller
     config.include FactoryGirl::Syntax::Methods
 
     config.before(:suite) do
