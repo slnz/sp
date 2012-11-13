d0d055914cc56a463a69c241cf8285b2bd8ffecc
Justin Sabelko <justin.sabelko@cojourners.com>
2012-11-12 21:54:27 -0600

    Update errors syntax for Rails 3

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -49,7 +49,7 @@ class PaymentsController < ApplicationController
                 @payment.approve!
                 # TODO: Send notification email
               else
-                @payment.errors.add_to_base("Credit card transaction failed: #{response.message}")
+                @payment.errors.add(:base, "Credit card transaction failed: #{response.message}")
                 #Send email this way instead of raising error in order to still give an error message to user.
                 # Notifier.deliver_notification('programmers@cojourners.com', # RECIPIENTS
                 #                     "sp_error@uscm.org", # FROM
