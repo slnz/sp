2d184948fbc86dcf2636b39381a465224129b39f
Josh Starcher <josh.starcher@gmail.com>
2013-10-11 12:40:43 -0400

    Remove config threadsafe

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -37,9 +37,6 @@ Sp2::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
 
-  # Enable threaded mode
-  config.threadsafe!
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
