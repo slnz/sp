da23a431e44cbbdc7cf86023a8c359d8ccfe788b
Josh Starcher <josh.starcher@gmail.com>
2013-10-11 10:48:44 -0400

    Use erb for image paths in css file

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Sp2
     #config.assets.precompile += [/(?:\/|\\|\A).*\.css/]
     config.assets.precompile += [/\A[a-z0-9]\w+.(css|js)\z/]
     config.assets.precompile += ['ie7-admin.css', 'admin_manifest.css', 'qe_admin.css']
-    config.assets.compile = true
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
