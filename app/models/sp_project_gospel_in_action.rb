20e804d00d876cd5bc0be573caf0e2b4ca33f4e6
Josh Starcher <josh.starcher@gmail.com>
2013-10-17 20:24:56 -0400

    Fix push to global registry

--- app/models/sp_project_gospel_in_action.rb
+++ app/models/sp_project_gospel_in_action.rb
@@ -7,6 +7,13 @@ class SpProjectGospelInAction < ActiveRecord::Base
   belongs_to :gospel_in_action, :class_name => "SpGospelInAction", :foreign_key => "gospel_in_action_id"
   belongs_to :project, :class_name => "SpProject", :foreign_key => "project_id"
 
+  def async_push_to_global_registry
+    attributes_to_push['project_id'] = project.global_registry_id if project && project.global_registry_id
+    attributes_to_push['gospel_in_action_id'] = gospel_in_action.global_registry_id if gospel_in_action && gospel_in_action.global_registry_id
+
+    super
+  end
+
   def self.skip_fields_for_gr
     %w[id created_at updated_at global_registry_id]
   end
