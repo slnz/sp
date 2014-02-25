4814c8aac5c313e086c8b1a71778f4eeeb419a93
Josh Starcher <josh.starcher@gmail.com>
2014-02-25 09:24:50 +0800

    Add spec for designation numbers

---
+++ spec/models/sp_designation_number_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe SpDesignationNumber do
+  before(:each) do
+  end
+
+  it "ensures that the designation_number is 7 digits" do
+    d = SpDesignationNumber.new(designation_number: '123456')
+    d.save
+    expect(d.designation_number).to eq('0123456')
+  end
+
+end
