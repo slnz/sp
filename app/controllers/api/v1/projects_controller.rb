2fee04b95234989db01e3bb21cdee8178f9b5b54
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2014-04-02 12:16:18 +0800

    Implement API

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::ProjectsController < Api::V1::BaseController
+
+  def index
+    projects = ProjectFilter.new(params[:filters]).filter(SpProject.all)
+    render render_options(projects, params[:order] || 'name')
+  end
+
+  def show
+    project = SpProject.find(params[:id])
+    render render_options(project)
+  end
+
+  private
+
+  def available_includes
+    []
+  end
+
+  def available_methods
+    [:applicants, :pd, :apd, :opd, :coordinator, :staff, :volunteers, :kids, :evaluators]
+  end
+end
---
+++ app/serializers/person_serializer.rb
@@ -0,0 +1,17 @@
+require_dependency 'cru_enhancements'
+class PersonSerializer < ActiveModel::Serializer
+  INCLUDES = [:phone_numbers, :email_addresses]
+  include CruEnhancements
+
+  attributes :id, :account_no, :last_name, :first_name, :middle_name, :preferred_name, :gender, :region,
+             :is_staff, :title, :campus, :university_state, :year_in_school, :major, :minor,
+             :marital_status, :is_child, :created_at, :updated_at, :created_by, :changed_by,
+             :birth_date, :date_became_christian, :graduation_date, :is_secure,
+             :ministry, :strategy, :fb_uid, :global_registry_id, :user_id
+
+  has_one :address
+
+  def address
+    object.current_address
+  end
+end
