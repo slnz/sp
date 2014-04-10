762d5a48290c168f12762aba9b21563dc14dd8e1
Justin Sabelko <justin.sabelko@cru.org>
2014-04-10 17:42:48 -0500

    Add reply-to to project email

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -1,11 +1,11 @@
 class ProjectMailer < ActionMailer::Base
   default :from => "Summer Projects <gosummerproject@cru.org>"
   
-  def team_email(to, from, cc, files, subject, body)
+  def team_email(to, from, reply_to, cc, files, subject, body)
     files.each do |file|
       attachments[file.original_filename] = File.read(file.tempfile)
     end
-    mail(:to => to, :from => from, :cc => cc, :subject => subject) do |format|
+    mail(:to => to, :from => from, :reply_to => reply_to, :cc => cc, :subject => subject) do |format|
       format.html {render :text => body}
     end
   end
