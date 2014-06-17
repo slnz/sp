f93ee395cd26478f84ed5a32219846ebaa0bbf01
Elikem Adadevoh <elikem@gmail.com>
2014-06-16 22:56:36 -0400

    adds specs for reports#show

--- spec/controllers/admin/reports_controller_spec.rb
+++ spec/controllers/admin/reports_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Admin::ReportsController do
+  let(:user) { create(:user) }
 
+  context '#show' do
+    it 'determines if you are a director' do
+      create(:sp_director, user: user)
+      session[:cas_user] = 'foo@example.com'
+      session[:user_id] = user.id
+
+      get :show
+
+      if SpUser.find_by_ssm_id(session[:user_id]).type == 'SpDirector'
+        response.should redirect_to(action: :director)
+      end
+    end
+  end
 end
