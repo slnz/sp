149521a001fd29e2eff100f0c36c156aec44c51f
Elikem Adadevoh <elikem@gmail.com>
2014-06-25 01:29:15 -0400

    adds specs for users#index

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Admin::UsersController do
+  let(:user) { create(:user, person: create(:person)) }
 
+  context '#index' do
+    it '' do
+      create(:sp_director, user: user, person_id: user.person.id)
+      session[:cas_user] = 'foo@example.com'
+      session[:user_id] = user.id
+
+      get :index, type: 'national'
+    end
+  end
 end
