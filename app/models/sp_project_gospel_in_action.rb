c8079fccedf8387b7483f04b04d0cbd4a8b23181
Josh Starcher <josh.starcher@gmail.com>
2014-06-25 15:02:40 -0400

    Fix pushing sp gospel in action relationships.

--- app/models/sp_project_gospel_in_action.rb
+++ app/models/sp_project_gospel_in_action.rb
@@ -1,24 +1,34 @@
-require_dependency 'global_registry_methods'
-
+require_dependency 'global_registry_relationship_methods'
 class SpProjectGospelInAction < ActiveRecord::Base
-  include GlobalRegistryMethods
   include Sidekiq::Worker
+  include GlobalRegistryRelationshipMethods
 
   belongs_to :gospel_in_action, :class_name => "SpGospelInAction", :foreign_key => "gospel_in_action_id"
   belongs_to :project, :class_name => "SpProject", :foreign_key => "project_id"
 
   def async_push_to_global_registry
-    attributes_to_push['project_id'] = project.global_registry_id if project && project.global_registry_id
-    attributes_to_push['gospel_in_action_id'] = gospel_in_action.global_registry_id if gospel_in_action && gospel_in_action.global_registry_id
-
+    return unless project && gospel_in_action
     super
   end
 
-  def self.skip_fields_for_gr
-    %w[id created_at updated_at global_registry_id]
+  def attributes_to_push
+    if global_registry_id
+      super
+    else
+      super('gospel_in_action', 'summer_project_gospel_in_action', gospel_in_action)
+    end
   end
 
-  def self.global_registry_entity_type_name
-    'summer_project_project_gospel_in_action'
+  def create_in_global_registry(base_object = nil, relationship_name = nil)
+    super(project, 'gospel_in_action')
+  end
+
+
+  def self.push_structure_to_global_registry
+    super(SpProject, SpGospelInAction, 'project', 'gospel_in_action')
+  end
+
+  def self.skip_fields_for_gr
+    %w[id created_at updated_at project_id gospel_in_action_id global_registry_id]
   end
 end
