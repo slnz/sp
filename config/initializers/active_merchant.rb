ace6483036e43f0349970168f3c48f7988c98ff3
Josh Starcher <josh.starcher@gmail.com>
2014-08-03 16:04:11 -0400

    Make sure authnet test mode only gets used in non-prod envs

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -2,4 +2,8 @@ GATEWAY = ActiveMerchant::Billing::AuthorizeNetGateway.new(
  :login => APP_CONFIG['merchant_login'],
  :password => APP_CONFIG['merchant_password']
 ) unless defined? GATEWAY
-ActiveMerchant::Billing::AuthorizeNetGateway.live_url = "https://test.authorize.net/gateway/transact.dll"
+
+unless Rails.env.production?
+  ActiveMerchant::Billing::AuthorizeNetGateway.live_url = "https://test.authorize.net/gateway/transact.dll"
+end
+
