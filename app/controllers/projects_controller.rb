f918e6d931f789425828cfc1bd162fd53c43d654
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-08-25 11:19:13 +1200

    removed filters for frontend

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,114 +16,22 @@ class ProjectsController < ApplicationController
   end
 
   def index
-    #@key = Digest::SHA1.hexdigest(params.collect {|k,v| [k,v]}.flatten.join('/') + '/' + request.format)
-    #unless fragment_exist?(@key)
-      unless params.size == 2
-        year = params[:year].present? ? params[:year].to_i : SpApplication.year
-        conditions = [[],[]]
-        conditions[0] << "#{SpProject.table_name}.show_on_website = 1"
-        conditions[0] << "(#{SpProject.table_name}.current_students_men + #{SpProject.table_name}.current_students_women) < (#{SpProject.table_name}.max_accepted_men + #{SpProject.table_name}.max_accepted_women)"
-        unless params[:all] == 'true'
-          if params[:id] && params[:id].present?
-            conditions[0] << "#{SpProject.table_name}.id IN(?)"
-            conditions[1] << params[:id]
-          end
-          if params[:name] && !params[:name].empty?
-            conditions[0] << "#{SpProject.table_name}.name like ?"
-            conditions[1] << "%#{params[:name]}%"
-          end
-          if params[:city] && !params[:city].empty?
-            conditions[0] << "#{SpProject.table_name}.city = ?"
-            conditions[1] << params[:city]
-          end
-          if params[:country] && !params[:country].empty?
-            countries = params[:country].split(',')
-            condition = []
-            countries.each do |country|
-              condition << "#{SpProject.table_name}.country LIKE ?"
-              conditions[1] << '%'+country+'%'
-            end
-            conditions[0] << '(' + condition.join(' OR ') + ')'
-          end
-          # this option has two modes of access to accomodate the form post and
-          # the xml feed. params[:project][:partner] is for the form post.
-          # params[:partner] is for the xml feed.
-          if (params[:partner] || (params[:project] && params[:project][:partner])) &&
-                !(partner = params[:partner] || params[:project][:partner]).empty?
-            conditions[0] << "(#{SpProject.table_name}.primary_partner = ? OR
-                               #{SpProject.table_name}.secondary_partner = ? OR
-                               #{SpProject.table_name}.tertiary_partner = ?)"
-            conditions[1] << partner
-            conditions[1] << partner
-            conditions[1] << partner
-          end
-          if params[:world_region] && !params[:world_region].empty?
-            world_regions = params[:world_region].split(',')
-            condition = []
-            world_regions.each do |world_region|
-              condition << "#{SpProject.table_name}.world_region LIKE ?"
-              conditions[1] << '%'+world_region+'%'
-            end
-            conditions[0] << '(' + condition.join(' OR ') + ')'
-          end
-          if params[:start_month].present?
-            day = params[:start_day].present? ? params[:start_day].to_i : 1
-            start_date = Time.mktime(year, params[:start_month].to_i, day)
-            conditions[0] << "#{SpProject.table_name}.start_date >= ?"
-            conditions[1] << start_date.to_s(:db)
-          end
+      year = params[:year].present? ? params[:year].to_i : SpApplication.year
+      conditions = [[],[]]
+      conditions[0] << "#{SpProject.table_name}.show_on_website = 1"
+      conditions[0] << "(#{SpProject.table_name}.current_students_men + #{SpProject.table_name}.current_students_women) < (#{SpProject.table_name}.max_accepted_men + #{SpProject.table_name}.max_accepted_women)"
+      conditions[0] = conditions[0].join(' AND ')
+      conditions.flatten!
 
-          if params[:end_month] && !params[:end_month].empty?
-            day = params[:end_day].present? ? params[:end_day].to_i : COMMON_YEAR_DAYS_IN_MONTH[params[:end_month].to_i]
-            end_date = Time.mktime(year, params[:end_month].to_i, day)
-            conditions[0] << "#{SpProject.table_name}.end_date <= ?"
-            conditions[1] << end_date
-          elsif params[:start_month].present?
-            end_date = Time.mktime(year, 12, 31)
-            conditions[0] << "#{SpProject.table_name}.end_date <= ?"
-            conditions[1] << end_date
-          end
-          if params[:project_type] && !params[:project_type].empty?
-            conditions[0] << "#{SpProject.table_name}" + get_project_type_condition
-          end
-          if params[:focus] && params[:focus].to_i != 0
-            focus = SpMinistryFocus.find(params[:focus])
-            build_focus_conditions(focus, conditions)
-          end
-          if params[:focus_name] && !params[:focus_name].empty?
-            focus = SpMinistryFocus.find_by_name(params[:focus_name])
-            build_focus_conditions(focus, conditions)
-          end
-          if params[:from_weeks] && !params[:from_weeks].empty?
-            conditions[0] << "#{SpProject.table_name}.weeks >= ?"
-            conditions[1] << params[:from_weeks]
-          end
-          if params[:to_weeks] && !params[:to_weeks].empty?
-            conditions[0] << "#{SpProject.table_name}.weeks <= ?"
-            conditions[1] << params[:to_weeks]
-          end
-          if params[:job] && !params[:job].empty?
-            conditions[0] << "#{SpProject.table_name}.job = ?"
-            conditions[1] << (params[:job] ? true : false)
-          end
-        end
-        conditions[0] = conditions[0].join(' AND ')
-        conditions.flatten!
+      if conditions[0].empty?
+        @projects = []
+      else
+        @searched = true
+      @projects = SpProject.current
+      @projects = @projects.where(conditions)
+                           .includes(:primary_ministry_focus, :ministry_focuses)
+                           .order('sp_projects.name, sp_projects.year')
 
-        if conditions[0].empty?
-          @projects = []
-        else
-          @searched = true
-          if params[:all] == 'true'
-            @projects = SpProject.current
-          else
-            @projects = SpProject.open
-          end
-          @projects = @projects.where(conditions)
-                               .includes(:primary_ministry_focus, :ministry_focuses)
-                               .order('sp_projects.name, sp_projects.year')
-        end
-      #end
       respond_to do |format|
         format.html do
           @projects.reject! {|p| !p.use_provided_application?} if @projects
