38b338c866f9288d0ea747711e311409e13d81ec
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-08-25 10:58:40 +1200

    moved redis configuration to env files

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,15 +3,13 @@ require 'sidekiq'
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-resque_config = YAML.load_file(rails_root + '/config/redis.yml')
-
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://' + resque_config[rails_env],
+  config.redis = { url: ENV['REDISCLOUD_URL'],
                    namespace: "SP:#{rails_env}:resque"}
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://' + resque_config[rails_env],
+  config.redis = { url: ENV['REDISCLOUD_URL'],
                    namespace: "SP:#{rails_env}:resque"}
   config.failures_default_mode = :exhausted
 end
