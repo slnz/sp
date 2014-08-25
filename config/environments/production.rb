1bbcb0fa8ac3f8db4f447cb6d9a62d5d9279c787
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-08-25 14:03:08 +1200

    added heroku send grid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,14 @@ Sp2::Application.configure do
                      :socket_timeout => 1.5,
                      :socket_failure_delay => 0.2
                     }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
