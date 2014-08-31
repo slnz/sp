a7218eaae572515e28e3547002040e0ec6f48b26
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-09-01 10:30:33 +1200

    fix asset compilation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.all
+//= require jquery-ui
 //= require bootstrap
 //= require qe/jquery.validate.pack.js
 //= require qe/jquery.metadata.js
