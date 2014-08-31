424ff99c56f92723a6ec2a72110fe646dfa3a4c4
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-09-01 10:30:17 +1200

    add key configuration for gr, new relic, relay

--- config/initializers/global_registry.rb
+++ config/initializers/global_registry.rb
@@ -1,7 +1,7 @@
 require 'global_registry'
 
 GlobalRegistry.configure do |config|
-  config.access_token = APP_CONFIG['global_registry_token']
-  config.base_url = APP_CONFIG['global_registry_url']
+  config.access_token = ENV['global_registry_token']
+  config.base_url = ENV['global_registry_url']
 end
 
--- config/initializers/relay_api_client.rb
+++ config/initializers/relay_api_client.rb
@@ -1,5 +1,5 @@
 RelayApiClient.configure do |config|
   config.wsdl = 'https://signin.ccci.org/sso/selfservice/webservice/5.0?wsdl'
-  config.linker_username = APP_CONFIG['linker_username']
-  config.linker_password = APP_CONFIG['linker_password']
+  config.linker_username = ENV['linker_username']
+  config.linker_password = ENV['linker_password']
 end
