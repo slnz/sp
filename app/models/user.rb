d18a33ccb8dc62d906cdd0af947dd53ac9e2bfd1
Andrew Roth <andrewroth@gmail.com>
2014-10-29 12:23:56 -0400

    forgot to remove binding.pry's

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ActiveRecord::Base
   def self.find_by_id(id) self.find_by_userID(id); end
 
   def self.find_or_create_from_cas(atts)
-    binding.pry
     # Look for a user with this guid
     guid = att_from_receipt(atts, 'ssoGuid')
     first_name = att_from_receipt(atts, 'firstName')
@@ -24,7 +23,6 @@ class User < ActiveRecord::Base
   end
 
   def self.find_or_create_from_guid_or_email(guid, email, first_name, last_name, secure = true)
-    binding.pry
     if guid
       u = ::User.where(:globallyUniqueID => guid).first
     end
@@ -49,7 +47,6 @@ class User < ActiveRecord::Base
     # make sure we have a person
     unless u.person
       # Attach the found person to the user, or create a new person
-      binding.pry
       u.person = ::Person.create!(:first_name => first_name,
                                   :last_name => last_name)
       u.person.fk_ssmUserId = u.id
