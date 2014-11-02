ab2ac3e142483bf8d0c04e4d9cfb31457264d772
Andrew Roth <andrewroth@gmail.com>
2014-11-01 21:59:07 -0400

    trying to fix double submit on payments

--- app/assets/javascripts/sp.js
+++ app/assets/javascripts/sp.js
@@ -1,22 +1,3 @@
-$(function() {
-    // Payment submission
-    $(document).on('click', '.submit_payment', function() {
-        var form = $(this).closest('form');
-        $.ajax({url: $(this).attr('data-url'), data: form.serializeArray(), type: 'POST', dataType:'script',
-            beforeSend: function (xhr) {
-                $('body').trigger('ajax:loading', xhr);
-            },
-            complete: function (xhr) {
-                $('body').trigger('ajax:complete', xhr);
-            },
-            error: function (xhr, status, error) {
-                $('body').trigger('ajax:failure', [xhr, status, error]);
-            }
-        });
-        return false;
-    });
-});
-
 function swapPayment(type) {
     $(".pay_view").hide();
     $('#pay_' + type).show();
