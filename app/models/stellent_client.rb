a904c3e152104f258125245560baf9cccb4dcd6d
Josh Starcher <josh.starcher@gmail.com>
2015-01-02 13:40:40 -0800

    Convert project_description to html

--- app/models/stellent_client.rb
+++ app/models/stellent_client.rb
@@ -37,7 +37,7 @@ class StellentClient
       d_doc_account: 'Shared-dss-staff-' + designation_number,
       primary_file: {
         file_name: designation_number + '-content.xml',
-        file_content: Base64.encode64(xml.gsub("\n",'').strip).gsub("\n",'')
+        file_content: Base64.encode64(xml(@app).gsub("\n",'').strip).gsub("\n",'')
       },
       extra_props: [
         { name: 'xHidden', value: 'FALSE' },
@@ -52,13 +52,13 @@ class StellentClient
     }
   end
 
-  def xml
-    picture = @app.project.picture_file_name ? clean('<img src="' + @app.project.picture.url + '" />') : ''
+  def xml(app)
+    picture = app.project.picture_file_name ? clean('<img src="' + app.project.picture.url + '" />') : ''
     <<-EOF
       <?xml version="1.0" encoding="utf-8"?>
       <wcm:root xmlns:wcm="http://www.stellant.com/wcm-data/ns/8.0.0" version="8.0.0.0">
-      <wcm:element name="title">#{clean(@app.person.informal_full_name)}</wcm:element>
-      <wcm:element name="body">#{clean(@app.project.full_project_description)}</wcm:element>
+      <wcm:element name="title">#{clean(app.person.informal_full_name)}</wcm:element>
+      <wcm:element name="body">#{clean('<p>' + app.project.full_project_description.gsub(/\n/, '<br>') + '</p>')}</wcm:element>
       <wcm:element name="show_homepage_headlines">true</wcm:element>
       <wcm:element name="show_page_tools">true</wcm:element>
       <wcm:element name="disable_facebook_comments">true</wcm:element>
