4efcafcc929829571ea6d6c89374efe059b69e18
Andrew Roth <andrewroth@gmail.com>
2015-01-11 15:09:45 -0500

    don't validate attachment type because they can be anything

--- app/decorators/models/fe/answer_decorator.rb
+++ app/decorators/models/fe/answer_decorator.rb
@@ -2,4 +2,5 @@ Fe::Answer.class_eval do
   has_attached_file :attachment, :storage => :s3,
     :s3_credentials => Rails.root.join("config/amazon_s3.yml"),
     :path => "sp/applications/:attachment/:id/:filename"
+  do_not_validate_attachment_file_type :attachment # these attachments can be any content type
 end
