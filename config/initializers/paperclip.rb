6b59850c16c097740a526181112d7c502dd21129
Andrew Roth <andrewroth@gmail.com>
2015-01-14 23:36:39 -0500

    fix 'Attachment has an extension that does not match its contents'

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,9 @@
+# https://github.com/thoughtbot/paperclip/issues/1470
+require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
