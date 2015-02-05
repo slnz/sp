95a84f18938e7db85ebfb8fbfcd4f163931c0ce5
Andrew Roth <andrewroth@gmail.com>
2015-02-04 22:22:13 -0500

    go with default mailing options since that seems to work for PR

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -53,7 +53,5 @@ Sp2::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :smtp
-
   config.action_mailer.default_url_options= { :host => 'sp.stage.uscm.org' }
 end
