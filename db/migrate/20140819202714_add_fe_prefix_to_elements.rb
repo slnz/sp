35d79db1d4e370d5af56343a8f1e67d35e00c8bb
Andrew Roth <andrewroth@gmail.com>
2015-02-20 13:31:17 -0500

    also add Fe:: prefix to AttachmentField kinds

--- db/migrate/20140819202714_add_fe_prefix_to_elements.rb
+++ db/migrate/20140819202714_add_fe_prefix_to_elements.rb
@@ -1,6 +1,6 @@
 class AddFePrefixToElements < ActiveRecord::Migration
   def change
-    %w(TextField Paragraph ChoiceField Section StateChooser DateField SchoolPicker ProjectPreference QuestionGrid ReferenceQuestion PaymentQuestion).each do |class_name|
+    %w(TextField Paragraph ChoiceField Section StateChooser DateField SchoolPicker ProjectPreference QuestionGrid ReferenceQuestion PaymentQuestion AttachmentField).each do |class_name|
       Fe::Element.where(kind: class_name).update_all(kind: "Fe::#{class_name}")
     end
   end
